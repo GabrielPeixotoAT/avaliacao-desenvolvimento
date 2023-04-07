9ce317f898f97829e8ff14b08aa88ffa7edac79e
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-07 14:54:03 -0300

    feat: implementação de menu para selecionar as questões

---
+++ AvaliacaoDesenvolvimento/Application/Menu/MenuEngine.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliacaoDesenvolvimento.Application.Menu
+{
+    public class MenuEngine
+    {
+        public int ItemMaxLength { get; set; } = 15;
+        List<string> menuItems;
+
+        public MenuEngine(List<string> menuItems) 
+        {
+            this.menuItems = menuItems;
+        }
+
+        public int RenderMenu()
+        {
+            List<string> sb = new List<string>();
+            int index = 0;
+            ConsoleKeyInfo aws;
+
+            Console.WriteLine("QUESTÕES: \n");
+
+            foreach (string option in menuItems)
+            {
+                string optionUpper = option.ToUpper();
+
+                for (int i = optionUpper.Length; i < ItemMaxLength; i++)
+                {
+                    optionUpper += " ";
+                }
+
+                sb.Add($" {optionUpper}");
+            }
+
+            do
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Clear();
+
+                Console.WriteLine("QUESTÕES: \n");
+
+                for (int i = 0; i < sb.Count; i++)
+                {
+                    if (i == index)
+                    {
+                        SelectedOption();
+                    }
+                    else
+                    {
+                        NonSelectedOption();
+                    }
+
+                    Console.WriteLine(sb[i]);
+                }
+
+                aws = Console.ReadKey();
+
+                switch (aws.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (index > 0)
+                            index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (index < sb.Count - 1)
+                            index++;
+                        break;
+                }
+            } while (aws.Key != ConsoleKey.Enter);
+
+            return index;
+        }
+
+        void SelectedOption()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        void NonSelectedOption()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
--- AvaliacaoDesenvolvimento/Program.cs
+++ AvaliacaoDesenvolvimento/Program.cs
@@ -1,6 +1,32 @@
 ﻿using AvaliacaoDesenvolvimento.Application;
+using AvaliacaoDesenvolvimento.Application.Menu;
 using AvaliacaoDesenvolvimento.Pergunta1;
 
-IPergunta pergunta = new Pergunta1();
+List<string> options = new List<string>();
 
-pergunta.Execute();
+options.Add("Pergunta 1");
+
+options.Add("Exit");
+
+MenuEngine menu = new MenuEngine(options);
+
+IPergunta pergunta;
+
+int select = 0;
+do
+{
+    select = menu.RenderMenu();
+
+    switch (select)
+    {
+        case 0:
+            pergunta = new Pergunta1();
+            Console.Clear();
+            pergunta.Execute();
+            break;
+    }
+
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Green;
+
+} while (select != options.Count() - 1);
